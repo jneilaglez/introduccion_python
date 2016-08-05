58a2cbd4e8c6678978b375c47199beb86099deaa
jneilaglez <jneilaglez@gmail.com>
2016-08-05 19:54:24 +0200

    License added to source files

--- 2_exercise.py
+++ 2_exercise.py
@@ -1,3 +1,20 @@
+"""
+    Copyright (C) 2016  Jesús Neila González jneilaglez@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 """ Module for practise python basics concepts: 2_exercise. """
 
 list1 = ["primero", 2, "3.5", 4.0, "ultimo"]
@@ -20,7 +37,7 @@ del list1[0]
 print(list1)
 
 """  Delete last 2 elements simultaneously. """
-del list1[len(list1) - 2:]
+del list1[2:]
 print(list1)
 
 """ Is list empty? """
